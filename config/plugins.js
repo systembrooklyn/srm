0a45c07ce39cb45374de4a2578e3275d8607109c
systembrooklyn <163738087+systembrooklyn@users.noreply.github.com>
2024-09-03 20:19:46 +0300

    3-9-2024

--- config/plugins.js
+++ config/plugins.js
@@ -1,2 +1,19 @@
-module.exports = () => ({});
-
+module.exports = ({ env }) => ({
+    email: {
+      provider: 'smtp', // نستخدم SMTP كـ provider لإرسال الإيميلات
+      providerOptions: {
+        host: 'smtp.gmail.com', // عنوان SMTP server - هنا بنستخدم Gmail
+        port: 587, // البورت الخاص بـ Gmail SMTP
+        secure: false, // False للـ TLS
+        auth: {
+          user: env('GMAIL_USER'), // البريد الإلكتروني اللي هيتم الإرسال منه
+          pass: env('GMAIL_PASS'), // كلمة المرور أو App password للبريد
+        },
+      },
+      settings: {
+        defaultFrom: env('GMAIL_USER'), // البريد اللي هتظهر منه الإيميلات
+        defaultReplyTo: env('GMAIL_USER'), // البريد اللي هيترد عليه
+      },
+    },
+  });
+  
