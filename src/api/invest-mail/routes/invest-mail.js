08ecf3466995185b3ec905087f7e15fe9640ed78
systembrooklyn <163738087+systembrooklyn@users.noreply.github.com>
2024-11-18 17:19:48 +0200

    make route in invest

--- src/api/invest-mail/routes/invest-mail.js
+++ src/api/invest-mail/routes/invest-mail.js
@@ -7,3 +7,18 @@
 const { createCoreRouter } = require('@strapi/strapi').factories;
 
 module.exports = createCoreRouter('api::invest-mail.invest-mail');
+
+module.exports = {
+    
+    routes: [
+      {
+        method: 'POST',
+        path: '/invest-mail',
+        handler: 'invest-mail.create',
+        config: {
+          policies: [],
+          middlewares: [],
+        },
+      },
+    ],
+}
