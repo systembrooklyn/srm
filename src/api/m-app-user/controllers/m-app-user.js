a021faa184d8dabf254ac0dc2158a93b22a860ba
systembrooklyn <163738087+systembrooklyn@users.noreply.github.com>
2024-11-21 18:07:18 +0200

    make custom fun in controller of m-app-user&routes

--- src/api/m-app-user/controllers/m-app-user.js
+++ src/api/m-app-user/controllers/m-app-user.js
@@ -1,9 +1,98 @@
-'use strict';
+"use strict";
 
 /**
  * m-app-user controller
  */
 
-const { createCoreController } = require('@strapi/strapi').factories;
+const { createCoreController } = require("@strapi/strapi").factories;
 
-module.exports = createCoreController('api::m-app-user.m-app-user');
+module.exports = createCoreController(
+  "api::m-app-user.m-app-user",
+  ({ strapi }) => ({
+    async checkCredentials(ctx) {
+      // التحقق من نوع الطلب
+      if (ctx.request.method !== "POST") {
+        return ctx.badRequest("This endpoint only accepts POST requests.");
+      }
+      const { email, password } = ctx.request.body;
+
+      if (!email || !password) {
+        return ctx.badRequest("Email and password are required.");
+      }
+
+      const user = await strapi.db.query("api::m-app-user.m-app-user").findOne({
+        where: { email: email },
+        select: ["email", "password", "name", "stID", "phone", "ppURL"],
+      });
+
+      if (!user) {
+        return ctx.send({
+          success: false,
+          message: "البريد الإلكتروني غير موجود.",
+        });
+      }
+
+      // مقارنة كلمة المرور مباشرة (غير موصى به)
+      if (password !== user.password) {
+        return ctx.send({ success: false, message: "كلمة المرور غير صحيحة." });
+      }
+
+      delete user.password;
+
+      return ctx.send({ success: true, message: "تسجيل الدخول ناجح.", user });
+    },
+
+    async create(ctx) {
+      const { name, stID, email, phone, password, ppURL } = ctx.request.body;
+      const response = await strapi.service("api::m-app-user.m-app-user").create({
+        data: {
+          name,
+          stID,
+          email,
+          phone,
+          password,
+          ppURL,
+        },
+      });
+      return ctx.send(response);
+    },
+
+    async find(ctx) {
+      const response = await strapi.service("api::m-app-user.m-app-user").find();
+      return ctx.send(response);
+    },
+
+    async findOne(ctx) {
+      const { id } = ctx.params;
+      const response = await strapi.service("api::m-app-user.m-app-user").findOne({
+        id,
+      });
+      return ctx.send(response);
+    },
+
+    async update(ctx) {
+      const { id } = ctx.params;
+      const { name, stID, email, phone, password, ppURL } = ctx.request.body;
+      const response = await strapi.service("api::m-app-user.m-app-user").update({
+        id,
+        data: {
+          name,
+          stID,
+          email,
+          phone,
+          password,
+          ppURL,
+        },
+      });
+      return ctx.send(response);
+    },
+
+    async delete(ctx) {
+      const { id } = ctx.params;
+      const response = await strapi.service("api::m-app-user.m-app-user").delete({
+        id,
+      });
+      return ctx.send(response);
+    },
+  })
+);
--- src/api/m-app-user/routes/m-app-user.js
+++ src/api/m-app-user/routes/m-app-user.js
@@ -1,9 +1,62 @@
-'use strict';
+"use strict";
 
 /**
  * m-app-user router
  */
 
-const { createCoreRouter } = require('@strapi/strapi').factories;
+const { createCoreRouter } = require("@strapi/strapi").factories;
 
-module.exports = createCoreRouter('api::m-app-user.m-app-user');
+module.exports = createCoreRouter("api::m-app-user.m-app-user");
+
+module.exports = {
+  routes: [
+    {
+      method: "GET",
+      path: "/m-app-users",
+      handler: "m-app-user.find",
+      config: {
+        policies: [],
+      },
+    },
+    {
+      method: "POST",
+      path: "/m-app-users",
+      handler: "m-app-user.create",
+      config: {
+        policies: [],
+      },
+    },
+    {
+      method: "PUT",
+      path: "/m-app-users/:id",
+      handler: "m-app-user.update",
+      config: {
+        policies: [],
+      },
+    },
+    {
+      method: "DELETE",
+      path: "/m-app-users/:id",
+      handler: "m-app-user.delete",
+      config: {
+        policies: [],
+      },
+    },
+    {
+      method: "GET",
+      path: "/m-app-users/:id",
+      handler: "m-app-user.findOne",
+      config: {
+        policies: [],
+      },
+    },
+    {
+      method: "POST",
+      path: "/m-app-users/check-credentials",
+      handler: "m-app-user.checkCredentials",
+      config: {
+        policies: [],
+      },
+    },
+  ],
+};
